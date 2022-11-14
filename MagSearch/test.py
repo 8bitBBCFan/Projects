b4552e07e4b73bcb00b0e8f6d17cc5b3515f7d5f
8bitBBCFan <vzon2005@gmail.com>
2022-11-14 15:59:28 +0100

    Minor changes

--- MagSearch/test.py
+++ MagSearch/test.py
@@ -1,17 +1,7 @@
-
-s = 'Elektor120_2017-6-Nov :  14 [7]  34 [3]  37 [6] '
-
-# get page number dependent on xpos
-def get_page(xpos, s):
-    def __scan(dr):
-        i = 0
-        while s[xpos+i].isdigit(): i += dr
-        return (xpos+i+1 if i!=0 and s[xpos+i]==' ' else -1)
-
-    if xpos<len(s) and s!='':
-        i_left, i_right = __scan(-1), __scan(+1)
-        if i_left!=-1 and i_right!=-1:
-            return int(s[i_left:i_right])
-    return(-1)
-
-print(get_page(27, s))
+from tkinter import *      
+root = Tk()      
+canvas = Canvas(root, width = 1000, height = 1000)     
+canvas.pack()      
+img = PhotoImage(file="/home/pi/Documents/MagPi+Magnifier.png")      
+canvas.create_image(20,20, anchor=NW, image=img)      
+mainloop()
